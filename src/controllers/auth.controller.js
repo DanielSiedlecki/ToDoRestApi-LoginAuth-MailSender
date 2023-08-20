96419856c55233d16b8bd7ee1013cd18dd193514
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-08-21 00:14:10 +0200

    Add validation changePassword

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -29,21 +29,26 @@ exports.loginUser = async (req, res) => {
 };
 
 exports.changePassword = async (req, res) => {
-  const userEmail = req.body.email;
-  const oldPassword = req.body.oldpassword;
-  const newPassword = req.body.newpassword;
+  try {
+    const userEmail = req.body.email;
+    const oldPassword = req.body.oldpassword;
+    const newPassword = req.body.newpassword;
 
-  User.findByUsername(userEmail, (err, user) => {
-    if (err) {
-      return res.status(500).send(err);
-    } else {
-      user.changePassword(oldPassword, newPassword, function (err) {
-        if (err) {
-          return res.status(500).send(err);
-        } else {
-          res.send("Successfully changed password");
-        }
+    const user = await User.findByUsername(userEmail);
+
+    if (oldPassword === newPassword) {
+      return res.status(400).json({
+        message: "The new password cannot be the same as the old password",
       });
     }
-  });
+
+    await user.changePassword(oldPassword, newPassword);
+
+    res.status(200).json({ message: "Successfully changed password" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while changing the password" });
+  }
 };
